f9d5632926e62630b4644b31951e7ef516795682
Murianwind <y2k001hj@gmail.com>
2020-11-01 09:11:08 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 33030,
-	"created_at": "Fri Oct 30 00:11:07 +0000 2020",
+	"tweets": 33032,
+	"created_at": "Sun Nov 01 00:11:07 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,378 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Sat Oct 31 01:09:54 +0000 2020",
+		"id": 1322345017248112600,
+		"id_str": "1322345017248112640",
+		"full_text": "서오릉 3km 걷기 성공.. 사랑의 장기 기증.. 어렵지 않아요.. 여러분도 동참해보세요.\n\n#장기기증의날 #2020초록리본걷기대회 #서울시 #리보니 #사랑의장기기증 @서오릉 https://t.co/SKgqzvpbwM",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			121
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "장기기증의날",
+					"indices": [
+						52,
+						59
+					]
+				},
+				{
+					"text": "2020초록리본걷기대회",
+					"indices": [
+						60,
+						73
+					]
+				},
+				{
+					"text": "서울시",
+					"indices": [
+						74,
+						78
+					]
+				},
+				{
+					"text": "리보니",
+					"indices": [
+						79,
+						83
+					]
+				},
+				{
+					"text": "사랑의장기기증",
+					"indices": [
+						84,
+						92
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/SKgqzvpbwM",
+					"expanded_url": "https://www.instagram.com/p/CG_SwKajbGwfkiNjI2CToceEShRChpE7j00coo0/?igshid=19isupnritjjr",
+					"display_url": "instagram.com/p/CG_SwKajbGwf…",
+					"indices": [
+						98,
+						121
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://instagram.com\" rel=\"nofollow\">Instagram</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 29951636,
+			"id_str": "29951636",
+			"name": "Murian Song",
+			"screen_name": "murianwind",
+			"location": "Dokdo, Korea",
+			"description": "Korea's first Testing evangelist, Testing process improvement consultant, TOC evangelist, Rapid Testing, User Testing",
+			"url": "http://t.co/51S44z8ELu",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/51S44z8ELu",
+							"expanded_url": "http://murianwind.blogspot.com",
+							"display_url": "murianwind.blogspot.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1491,
+			"friends_count": 274,
+			"listed_count": 155,
+			"created_at": "Thu Apr 09 09:15:28 +0000 2009",
+			"favourites_count": 1595,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 32908,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/788229921/twitterProfilePhoto_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/788229921/twitterProfilePhoto_normal.jpg",
+			"profile_link_color": "93A644",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": {
+			"type": "Point",
+			"coordinates": [
+				37.6356,
+				126.8944
+			]
+		},
+		"coordinates": {
+			"type": "Point",
+			"coordinates": [
+				126.8944,
+				37.6356
+			]
+		},
+		"place": {
+			"id": "013a5e5135a1557e",
+			"url": "https://api.twitter.com/1.1/geo/id/013a5e5135a1557e.json",
+			"place_type": "city",
+			"name": "고양시",
+			"full_name": "대한민국 고양시",
+			"country_code": "KR",
+			"country": "대한민국",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							126.680816880228,
+							37.5746931669841
+						],
+						[
+							126.993029017475,
+							37.5746931669841
+						],
+						[
+							126.993029017475,
+							37.7269740012511
+						],
+						[
+							126.680816880228,
+							37.7269740012511
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": true,
+		"lang": "ko",
+		"text": "서오릉 3km 걷기 성공.. 사랑의 장기 기증.. 어렵지 않아요.. 여러분도 동참해보세요.\n\n#장기기증의날 #2020초록리본걷기대회 #서울시 #리보니 #사랑의장기기증 @서오릉 https://t.co/SKgqzvpbwM"
+	},
+	{
+		"created_at": "Sat Oct 31 01:00:14 +0000 2020",
+		"id": 1322342582295343000,
+		"id_str": "1322342582295343104",
+		"full_text": "사진을 찍어본적이 거의 없어서 무지 어색하네요.. 3km 걷기 도전한 곳은 바로.. 서오릉.. 숙종과 장희빈의 이야기를 들을 수 있는 곳입니다.\n\n#장기기증의날 #2020초록리본걷기대회 #서울시 #리보니 #사랑의장기기증 @서오릉 https://t.co/uG6YRl86bS",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			151
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "장기기증의날",
+					"indices": [
+						82,
+						89
+					]
+				},
+				{
+					"text": "2020초록리본걷기대회",
+					"indices": [
+						90,
+						103
+					]
+				},
+				{
+					"text": "서울시",
+					"indices": [
+						104,
+						108
+					]
+				},
+				{
+					"text": "리보니",
+					"indices": [
+						109,
+						113
+					]
+				},
+				{
+					"text": "사랑의장기기증",
+					"indices": [
+						114,
+						122
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/uG6YRl86bS",
+					"expanded_url": "https://www.instagram.com/p/CG_RpYADnxEd1xId736bJs1oHsmZfR6zJ0T9KQ0/?igshid=11vul1jjf272d",
+					"display_url": "instagram.com/p/CG_RpYADnxEd…",
+					"indices": [
+						128,
+						151
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://instagram.com\" rel=\"nofollow\">Instagram</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 29951636,
+			"id_str": "29951636",
+			"name": "Murian Song",
+			"screen_name": "murianwind",
+			"location": "Dokdo, Korea",
+			"description": "Korea's first Testing evangelist, Testing process improvement consultant, TOC evangelist, Rapid Testing, User Testing",
+			"url": "http://t.co/51S44z8ELu",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/51S44z8ELu",
+							"expanded_url": "http://murianwind.blogspot.com",
+							"display_url": "murianwind.blogspot.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1491,
+			"friends_count": 274,
+			"listed_count": 155,
+			"created_at": "Thu Apr 09 09:15:28 +0000 2009",
+			"favourites_count": 1595,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 32908,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/788229921/twitterProfilePhoto_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/788229921/twitterProfilePhoto_normal.jpg",
+			"profile_link_color": "93A644",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": {
+			"type": "Point",
+			"coordinates": [
+				37.6356,
+				126.8944
+			]
+		},
+		"coordinates": {
+			"type": "Point",
+			"coordinates": [
+				126.8944,
+				37.6356
+			]
+		},
+		"place": {
+			"id": "013a5e5135a1557e",
+			"url": "https://api.twitter.com/1.1/geo/id/013a5e5135a1557e.json",
+			"place_type": "city",
+			"name": "고양시",
+			"full_name": "대한민국 고양시",
+			"country_code": "KR",
+			"country": "대한민국",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							126.680816880228,
+							37.5746931669841
+						],
+						[
+							126.993029017475,
+							37.5746931669841
+						],
+						[
+							126.993029017475,
+							37.7269740012511
+						],
+						[
+							126.680816880228,
+							37.7269740012511
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": true,
+		"lang": "ko",
+		"text": "사진을 찍어본적이 거의 없어서 무지 어색하네요.. 3km 걷기 도전한 곳은 바로.. 서오릉.. 숙종과 장희빈의 이야기를 들을 수 있는 곳입니다.\n\n#장기기증의날 #2020초록리본걷기대회 #서울시 #리보니 #사랑의장기기증 @서오릉 https://t.co/uG6YRl86bS"
+	},
+	{
 		"created_at": "Thu Oct 29 05:05:32 +0000 2020",
 		"id": 1321679539219488800,
 		"id_str": "1321679539219488769",
