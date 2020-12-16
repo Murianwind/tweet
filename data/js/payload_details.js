beb1164ebae6b1d9ab2d507e076d7787f57b549b
Murianwind <y2k001hj@gmail.com>
2020-12-16 09:11:09 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 33063,
-	"created_at": "Tue Dec 15 00:11:07 +0000 2020",
+	"tweets": 33064,
+	"created_at": "Wed Dec 16 00:11:07 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,119 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Tue Dec 15 23:38:40 +0000 2020",
+		"id": 1338991899005255700,
+		"id_str": "1338991899005255681",
+		"full_text": "별 기대 없이 봤는데.. 나름 신선했다.. 그나저나 고질라는 여기저기 끊임 없이 나오는구나.. &lt;고질라 괴수행성&gt; #WATCHAPEDIA https://t.co/12O90TUi32",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			106
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "WATCHAPEDIA",
+					"indices": [
+						70,
+						82
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/12O90TUi32",
+					"expanded_url": "https://pedia.watcha.com/ko-KR/contents/mOAZGXQ",
+					"display_url": "pedia.watcha.com/ko-KR/contents…",
+					"indices": [
+						83,
+						106
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://pedia.watcha.com\" rel=\"nofollow\">WATCHA PEDIA</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 29951636,
+			"id_str": "29951636",
+			"name": "Murian Song",
+			"screen_name": "murianwind",
+			"location": "Dokdo, Korea",
+			"description": "Korea's first Testing evangelist, Testing process improvement consultant, TOC evangelist, Rapid Testing, User Testing",
+			"url": "http://t.co/51S44z8ELu",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/51S44z8ELu",
+							"expanded_url": "http://murianwind.blogspot.com",
+							"display_url": "murianwind.blogspot.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1486,
+			"friends_count": 274,
+			"listed_count": 156,
+			"created_at": "Thu Apr 09 09:15:28 +0000 2009",
+			"favourites_count": 1605,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 32949,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/788229921/twitterProfilePhoto_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/788229921/twitterProfilePhoto_normal.jpg",
+			"profile_link_color": "93A644",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": true,
+		"lang": "ko",
+		"text": "별 기대 없이 봤는데.. 나름 신선했다.. 그나저나 고질라는 여기저기 끊임 없이 나오는구나.. &lt;고질라 괴수행성&gt; #WATCHAPEDIA https://t.co/12O90TUi32"
+	},
+	{
 		"created_at": "Mon Dec 14 13:17:01 +0000 2020",
 		"id": 1338473067572461600,
 		"id_str": "1338473067572461568",
