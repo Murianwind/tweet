8ad37f1f179da36eaf392a46c789d2511787b93f
Murianwind <y2k001hj@gmail.com>
2022-01-26 09:11:08 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 33288,
-	"created_at": "Tue Jan 25 00:11:06 +0000 2022",
+	"tweets": 33289,
+	"created_at": "Wed Jan 26 00:11:07 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,155 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Tue Jan 25 07:00:58 +0000 2022",
+		"id": 1485870293851996200,
+		"id_str": "1485870293851996160",
+		"full_text": "공약 대결 - 내가 더 선호하는 공약을 낸 후보는? https://t.co/bgJA9XtTXc #심리테스트 #심테 #테스트 #퀴즈 #도다 #Made_With_Doda",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			92
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "심리테스트",
+					"indices": [
+						53,
+						59
+					]
+				},
+				{
+					"text": "심테",
+					"indices": [
+						60,
+						63
+					]
+				},
+				{
+					"text": "테스트",
+					"indices": [
+						64,
+						68
+					]
+				},
+				{
+					"text": "퀴즈",
+					"indices": [
+						69,
+						72
+					]
+				},
+				{
+					"text": "도다",
+					"indices": [
+						73,
+						76
+					]
+				},
+				{
+					"text": "Made_With_Doda",
+					"indices": [
+						77,
+						92
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/bgJA9XtTXc",
+					"expanded_url": "https://doda.app/quiz/niJeRgWsKF",
+					"display_url": "doda.app/quiz/niJeRgWsKF",
+					"indices": [
+						29,
+						52
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 29951636,
+			"id_str": "29951636",
+			"name": "Murian Song",
+			"screen_name": "murianwind",
+			"location": "Dokdo, Korea",
+			"description": "Korea's first Testing evangelist, Testing process improvement consultant, TOC evangelist, Rapid Testing, User Testing",
+			"url": "http://t.co/51S44z8ELu",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/51S44z8ELu",
+							"expanded_url": "http://murianwind.blogspot.com",
+							"display_url": "murianwind.blogspot.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1451,
+			"friends_count": 270,
+			"listed_count": 153,
+			"created_at": "Thu Apr 09 09:15:28 +0000 2009",
+			"favourites_count": 1623,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 33228,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/788229921/twitterProfilePhoto_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/788229921/twitterProfilePhoto_normal.jpg",
+			"profile_link_color": "93A644",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": true,
+		"lang": "ko",
+		"text": "공약 대결 - 내가 더 선호하는 공약을 낸 후보는? https://t.co/bgJA9XtTXc #심리테스트 #심테 #테스트 #퀴즈 #도다 #Made_With_Doda"
+	},
+	{
 		"created_at": "Mon Jan 24 13:34:57 +0000 2022",
 		"id": 1485599780416135200,
 		"id_str": "1485607055503556609",
