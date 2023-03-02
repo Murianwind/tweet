40a2ed031a8f01dcf0465545455bee75f39572e9
Murian <y2k001hj@gmail.com>
2023-03-02 09:11:07 +0900

    Added 1 tweets

---
+++ data/js/tweets/2023_03.js
@@ -0,0 +1,145 @@
+Grailbird.data.tweets_2023_03 = 
+[
+	{
+		"created_at": "Wed Mar 01 14:57:54 +0000 2023",
+		"id": 1630945462223253500,
+		"id_str": "1630945462223253504",
+		"full_text": "당헌 25조 당대표가 대통령 출마 할 경우 1년전 사임 조건에 예외 규정(전임 대통령 조기 사퇴) 즉시 넣어주세요. https://t.co/JkzTKyRdkT ##검찰독재 #촛불혁명 #윤석열퇴진 #당대표1년전사임조건 #촛불행동",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			126
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "검찰독재",
+					"indices": [
+						90,
+						95
+					]
+				},
+				{
+					"text": "촛불혁명",
+					"indices": [
+						96,
+						101
+					]
+				},
+				{
+					"text": "윤석열퇴진",
+					"indices": [
+						102,
+						108
+					]
+				},
+				{
+					"text": "당대표1년전사임조건",
+					"indices": [
+						109,
+						120
+					]
+				},
+				{
+					"text": "촛불행동",
+					"indices": [
+						121,
+						126
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/JkzTKyRdkT",
+					"expanded_url": "https://petitions.theminjoo.kr/23035005WDPDHTZ",
+					"display_url": "petitions.theminjoo.kr/23035005WDPDHTZ",
+					"indices": [
+						65,
+						88
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 29951636,
+			"id_str": "29951636",
+			"name": "Murian Song",
+			"screen_name": "murianwind",
+			"location": "Dokdo, Korea",
+			"description": "Korea's first Testing evangelist, Testing process improvement consultant, TOC evangelist, Rapid Testing, User Testing",
+			"url": "http://t.co/51S44z8ELu",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/51S44z8ELu",
+							"expanded_url": "http://murianwind.blogspot.com",
+							"display_url": "murianwind.blogspot.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1420,
+			"friends_count": 268,
+			"listed_count": 147,
+			"created_at": "Thu Apr 09 09:15:28 +0000 2009",
+			"favourites_count": 1643,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 33399,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/788229921/twitterProfilePhoto_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/788229921/twitterProfilePhoto_normal.jpg",
+			"profile_link_color": "93A644",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": true,
+		"lang": "ko",
+		"text": "당헌 25조 당대표가 대통령 출마 할 경우 1년전 사임 조건에 예외 규정(전임 대통령 조기 사퇴) 즉시 넣어주세요. https://t.co/JkzTKyRdkT ##검찰독재 #촛불혁명 #윤석열퇴진 #당대표1년전사임조건 #촛불행동"
+	}
+]
